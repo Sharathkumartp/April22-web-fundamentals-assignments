437198785205aabde3d33d5a4bed95c1c3c73e86
Sharath Kumar T P <sharathkumartp18@gmail.com>
2022-06-05 20:34:51 +0530

    june_1st_test

---
+++ June/June_1st_test/cubeofnum.js
@@ -0,0 +1,4 @@
+let n=parseInt(prompt('Enter the num:'))
+for(let i=1;i<=n;i++){
+    console.log(i*i*i)
+}
---
+++ June/June_1st_test/noddnum.js
@@ -0,0 +1,7 @@
+let n=parseInt(prompt('Enter the num:'))
+let sum=0;
+for(let i=1;i<=n;i++){
+    console.log(2*i-1)
+    sum += 2*i-1
+}
+console.log(sum);
